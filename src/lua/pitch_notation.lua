39eecb0dce2df08031ec0d1741238a050801acc2
Edison Moreland <edison@devdude.net>
2024-02-29 21:52:07 -0800

    organize

--- src/lua/pitch_notation.lua
+++ src/lua/pitch_notation.lua
@@ -1,3 +1,8 @@
+--- Standard tuning in western music.
+--- 12 notes of equal temperament with the root note A4 at 440hz
+A440_12TET = Tuning.new(440, 12)
+
+--- Give a name to all 12
 C = 0
 Cs = 1
 Df = 1
@@ -15,6 +20,16 @@ A = 9
 As = 10
 Bf = 10
 B = 11
+
+--- Returns the pitch of the given note, Ex: pitch(C, 4)
+---@param note number semitone offset from C in the given octave
+---@param octave number octave offset
+---@return number
+function pitch(note, octave)
+    local C0 = -((4 * 12) + 9)
+
+    return A440_12TET:note((C0 + (12 * octave)) + note)
+end
 
 local w = 2
 local h = 1
@@ -34,20 +49,6 @@ function scale(root, steps)
     end
 
     return scale
-end
-
---- Standard tuning in western music.
---- 12 notes of equal temperament with the root note A4 at 440hz
-A440_12TET = Tuning.new(440, 12)
-
---- Returns the pitch of the given note, Ex: pitch(C, 4)
----@param note number semitone offset from C in the given octave
----@param octave number octave offset
----@return number
-function pitch(note, octave)
-    local C0 = -((4 * 12) + 9)
-
-    return A440_12TET:note((C0 + (12 * octave)) + note)
 end
 
 ---@type table<string, number>
